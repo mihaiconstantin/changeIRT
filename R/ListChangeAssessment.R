7ee376dcac1d204823993d340d9e1ded744e8e1e
mihaiconstantin <c.tinmihai@ymail.com>
2017-03-15 12:21:19 +0100

    changed return type for ListChangeAssessment.R

--- R/ListChangeAssessment.R
+++ R/ListChangeAssessment.R
@@ -2,6 +2,6 @@ ChangeAssessment = list(
 	
 	# thetaPre: matrix where column one is the ability and column 2 the se
 	wald = function(thetaPre, thetaPost) {
-		(thetaPost[, 1] - thetaPre[, 1]) / sqrt((thetaPre[, 2] ^ 2) + (thetaPost[, 2] ^ 2))
+		cbind(wald = (thetaPost[, 1] - thetaPre[, 1]) / sqrt((thetaPre[, 2] ^ 2) + (thetaPost[, 2] ^ 2)), p = NA)
 	}
 )
