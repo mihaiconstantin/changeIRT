9ea2747e1f912119ce9200a6977e2249f3227ee4
mihaiconstantin <c.tinmihai@ymail.com>
2017-03-15 12:00:20 +0100

    Added dummy documentation

--- R/Functions.R
+++ R/Functions.R
@@ -1,3 +1,14 @@
+#' Simulate IRT data
+#'
+#' Works for 2PLM and GRM
+#'
+#' @param theta vector
+#' @param b vector or matrix (if matrix GRM is used)
+#' @param a vector (of equal size with \code{length(b)})
+#'
+#' @return children of class Simulator (to be added...)
+#'
+#' @export
 simulateirt = function(theta, b, a) {
 	if(is.matrix(b)) {
 		return(SimulatorGRM$new(theta, b, a))
@@ -6,15 +17,48 @@ simulateirt = function(theta, b, a) {
 	}
 }
 
+
+
+#' Estimate IRT model based on data
+#'
+#' Works for 2PLM and GRM - decide the dependency: ltm or mirt?
+#'
+#' @param data matrix
+#'
+#' @return children of class Estimator (to be added...)
+#'
+#' @export
 estimateirt = function(data) {
 	# check of the data is binary and return
 	# the appropriate IRT model
 }
 
-changeirt = function(theta.t1, theta.t2) {
+
+
+#' Compute RCI 
+#'
+#' to be added...
+#'
+#' @param theta.t1 matrix: fir column thetas, second column se
+#' @param theta.t2 matrix: fir column thetas, second column se
+#' @param method character: the test used; defaults to Wald test
+#'
+#' @return children of class ChangeAssessment (to be added...)
+#'
+#' @export
+changeirt = function(theta.t1, theta.t2, method = "wald") {
 	return(ChangeAssessment$wald(theta.t1, theta.t2))
 }
 
+
+
+#' Compute Person-Fit statistics
+#'
+#' to be added... decide dependecy or implementation
+#'
+#' @return matrix: first column test value, second column p value
+#'
+#' @export
 personfit = function() {
 	
 }
